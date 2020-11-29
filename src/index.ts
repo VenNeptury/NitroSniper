a1dd6690941057d3bebdbede0209d0e849d90847
Venny <sgtmattis@gmail.com>
2020-11-29 23:14:46 +0100

    Add multi slave support, add logging via webhook

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,48 @@
-import { Client, Message, TextChannel } from 'discord.js';
+import { Client, Collection, Message } from 'discord.js';
 import fetch from 'node-fetch';
-import { token, throttling, logChannel } from './config.json';
+import { token, throttling, webhook, slaves, userAgent } from './config.json';
 
 const nitroMatcher = /(discord.com\/gifts\/|discordapp.com\/gifts\/|discord.gift\/)([a-zA-Z0-9]+)/i;
+
 const snipedCodes = new Set<string>();
 let claims = 0;
 
-const client = new Client();
+const clients = new Collection<string, Client>();
 
-client.once('ready', () => log(`Successfully logged in as ${client.user.tag}\nSniping Nitro on ${client.guilds.size} servers`));
+if (!slaves.length) {
+	console.warn('Please specify some slaves and try again');
+	process.exit();
+}
 
-client.on('raw', (packet: any) => {
-	if ('MESSAGE_UPDATE' !== packet.t) return;
-	const channel = client.channels.get(packet.d.channel_id) as TextChannel;
-	if (!channel) return;
-	if (channel.messages.has(packet.d.id)) return;
-	channel
-		.fetchMessage(packet.d.id)
-		.then(msg => client.emit('messageUpdate', null, msg))
-		.catch(() => null);
-});
+for (const slaveToken of slaves) {
+	const client = new Client({
+		ws: {
+			// @ts-ignore
+			$os: '',
+			$browser: '',
+			$device: ''
+		}
+	});
 
-client.on('message', handleMessage);
-client.on('messageUpdate', (_, msg) => handleMessage(msg));
+	initHandlers(client);
+	client.once('ready', () => {
+		client.user.setStatus('invisible');
+		log(`Successfully initiated slave ${client.user.tag} ${client.user.bot ? '(BOT)' : '(USER)'} with ${client.guilds.size} servers`, client);
+	});
+
+	client.login(slaveToken);
+	clients.set(slaveToken, client);
+}
+
+function initHandlers(client: Client) {
+	client.on('message', handleMessage);
+	client.on('messageUpdate', (_, msg) => handleMessage(msg));
+}
+
+function clearHandlers(client: Client) {
+	client.removeListener('message', handleMessage);
+	client.removeListener('messageUpdate', handleMessage);
+}
 
 async function handleMessage(msg: Message) {
 	const match = msg.content.match(nitroMatcher);
@@ -36,20 +56,15 @@ async function handleMessage(msg: Message) {
 		body: JSON.stringify({ channel_id: null, payment_source_id: null }),
 		headers: {
 			authorization: token,
-			'user-agent':
-				'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) discord/0.0.308 Chrome/78.0.3904.130 Electron/7.3.2 Safari/537.36',
-			'content-type': 'application/json',
-			acccept: '*/*',
-			'accept-language': 'en-GB',
-			'accept-encoding': 'gzip, deflate, br',
-			referer: msg.guild ? `https://discord.com/channels/${msg.guild.id}/${msg.channel.id}` : ''
+			'user-agent': userAgent,
+			'content-type': 'application/json'
 		}
-	}).catch(() => null);
+	}).catch(() => void 0);
 	if (!res) return;
 
 	const json = await res.json();
 
-	log(`${new Date().toLocaleTimeString()} - Found code ${code} on ${msg.guild?.name || msg.author.username}. ${json.message}`);
+	log(`Found code \`${code}\` on \`${msg.guild?.name || msg.author.username}\`. ${json.message}`, msg.client);
 
 	if (json.message.includes('nitro')) {
 		claims++;
@@ -58,20 +73,34 @@ async function handleMessage(msg: Message) {
 		if (claims >= throttling.maxClaims) {
 			log(`Reached maximum amount of claims. Idling for ${throttling.cooldownInHours} hours.`);
 			claims = 0;
-			client.removeListener('message', handleMessage);
-			client.removeListener('messageUpdate', handleMessage);
+			clients.forEach(clearHandlers);
 			setTimeout(() => {
 				log(`Finished idling. Looking for codes again.`);
-				client.on('message', handleMessage);
-				client.on('messageUpdate', (_, msg) => handleMessage(msg));
+				clients.forEach(initHandlers);
 			}, 1000 * 60 * 60 * throttling.cooldownInHours);
 		}
 	}
 }
 
-function log(msg: string) {
-	console.log(msg);
-	if (logChannel) (client.channels.get(logChannel) as TextChannel)?.send(msg).catch(() => null);
-}
+function log(msg: string, slave?: Client) {
+	console.log(`${new Date().toLocaleTimeString()} - ${msg}`);
 
-client.login(token);
+	if (webhook)
+		fetch(webhook, {
+			method: 'POST',
+			body: JSON.stringify({
+				username: 'Nitro Sniper',
+				avatar_url: slave?.user.displayAvatarURL,
+				embeds: [
+					{
+						description: msg,
+						timestamp: new Date().toISOString(),
+						author: slave ? { name: slave.user.tag, icon_url: slave.user.displayAvatarURL } : null
+					}
+				]
+			}),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		}).catch(() => void 0);
+}
